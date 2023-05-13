b355f767250387415f946d5d20fe7dc447fae360
nicfv <nfmventura@gmail.com>
2023-05-13 13:00:01 -0700

    Add basic credentials in HTTP header, and a script to change the password

--- server/server.py
+++ server/server.py
@@ -1,6 +1,6 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 import json
-from server.setup import DIR_CLIENT, REL_FILE_ROOM
+from server.setup import DIR_CLIENT, REL_FILE_ROOM, PASSWORD
 
 
 class RequestHandler(SimpleHTTPRequestHandler):
@@ -8,20 +8,26 @@ class RequestHandler(SimpleHTTPRequestHandler):
         super().__init__(*args, directory=DIR_CLIENT, **kwargs)
 
     def do_POST(self):
-        if self.path[1:] in REL_FILE_ROOM:
-            payload = json.loads(self.rfile.read(
-                int(self.headers.get('Content-Length'))))
-            with open(self.path[1:], 'w') as f:
-                json.dump(payload, f)
-            self.send_response(200)
-            self.send_header('Content-Type', 'application/json')
-            self.end_headers()
-            self.wfile.write(b'{"status":200}')
+        if self.headers.get('password') == PASSWORD:
+            if self.path[1:] in REL_FILE_ROOM:
+                payload = json.loads(self.rfile.read(
+                    int(self.headers.get('Content-Length'))))
+                with open(self.path[1:], 'w') as f:
+                    json.dump(payload, f)
+                self.send_response(200)
+                self.send_header('Content-Type', 'application/json')
+                self.end_headers()
+                self.wfile.write(b'{"status":200}')
+            else:
+                self.send_response(400)
+                self.send_header('Content-Type', 'application/json')
+                self.end_headers()
+                self.wfile.write(b'{"status":400}')
         else:
-            self.send_response(400)
+            self.send_response(401)
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
-            self.wfile.write(b'{"status":400}')
+            self.wfile.write(b'{"status":401}')
 
 
 def startServer(port: int = 8000):
--- server/setup.py
+++ server/setup.py
@@ -11,6 +11,7 @@ FILE_ROOM = 'room.json'
 FILE_LOCAL = 'news-local.json'
 FILE_NATIONAL = 'news-national.json'
 FILE_WEATHER = 'weather.json'
+FILE_PASSWORD = 'password.txt'
 
 
 REL_FILE_ROOM = os.path.join(DIR_DATA, FILE_ROOM)
@@ -23,9 +24,12 @@ ABS_FILE_NATIONAL = os.path.join(DIR_CLIENT, DIR_API, FILE_NATIONAL)
 ABS_FILE_WEATHER = os.path.join(DIR_CLIENT, DIR_API, FILE_WEATHER)
 ABS_FILE_SCHEMA_ROOM = os.path.join(DIR_SERVER, DIR_SCHEMA, FILE_ROOM)
 ABS_FILE_SCHEMA_CONFIG = os.path.join(DIR_SERVER, DIR_SCHEMA, FILE_CONFIG)
+ABS_FILE_PASSWORD = os.path.join(DIR_SERVER, FILE_PASSWORD)
+PASSWORD = 'admin'
 
 
 def makeFiles():
+    global PASSWORD
     os.makedirs(ABS_DIR_API, exist_ok=True)
     os.makedirs(ABS_DIR_DATA, exist_ok=True)
     if not os.path.exists(ABS_FILE_ROOM):
@@ -34,3 +38,11 @@ def makeFiles():
     if not os.path.exists(ABS_FILE_CONFIG):
         with open(ABS_FILE_CONFIG, 'w') as f:
             f.write('{ "$schema": "../' + ABS_FILE_SCHEMA_CONFIG + '" }')
+    if os.path.exists(ABS_FILE_PASSWORD):
+        with open(ABS_FILE_PASSWORD, 'r') as f:
+            PASSWORD = f.read()
+        print(PASSWORD)
+    else:
+        with open(ABS_FILE_PASSWORD, 'w') as f:
+            f.write(PASSWORD)
+        print('Setting default server password: ' + PASSWORD)
